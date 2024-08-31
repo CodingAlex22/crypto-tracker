96099b014c19a3d7a69d896ae650dbd31ab007d3
Alex Nunez <106489406+CodingAlex22@users.noreply.github.com>
2024-08-30 19:25:00 -0700

    Update Chart.js

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -10,7 +10,7 @@ function Chart() {
   const chart = React.useRef();
  
   const getData = async () => {
-    const resp = await fetch(`https://coingecko-proxy-server2-1.onrender.com/${params.coinId}/365`);
+    const resp = await fetch(`https://https://test-proxy-1ynu.onrender.com/${params.coinId}/365`);
     const data = await resp.json();
     return data;
   }
